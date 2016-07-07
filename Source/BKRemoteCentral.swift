5b1fffa1f1d2e589b994c6c6b601f03d80489f2b
Caleb Kleveter <caleb.kleveter@gmail.com>
2016-07-07 08:46:19 -0500

    Updated to Swift 3

--- Source/BKRemoteCentral.swift
+++ Source/BKRemoteCentral.swift
@@ -29,20 +29,20 @@ import CoreBluetooth
     Representation of a remote central.
 */
 public class BKRemoteCentral: BKRemotePeer {
-
+    
     // MARK: Properties
-
+    
     internal let central: CBCentral
-
+    
     override internal var maximumUpdateValueLength: Int {
         return central.maximumUpdateValueLength
     }
-
+    
     // MARK: Initialization
-
+    
     internal init(central: CBCentral) {
         self.central = central
         super.init(identifier: central.identifier)
     }
-
+    
 }
