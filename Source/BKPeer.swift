477d59d07e23f2ab7599130d8ddb511c9f90a2ef
Caleb Kleveter <caleb.kleveter@gmail.com>
2016-07-07 08:44:40 -0500

    Updated to Swift 3

--- Source/BKPeer.swift
+++ Source/BKPeer.swift
@@ -1,60 +1,37 @@
 //
+//  BKPeer.swift
 //  BluetoothKit
 //
-//  Copyright (c) 2015 Rasmus Taulborg Hummelmose - https://github.com/rasmusth
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
+//  Created by Rasmus Taulborg Hummelmose on 13/05/2016.
+//  Copyright © 2016 Rasmus Taulborg Hummelmose. All rights reserved.
 //
 
 import Foundation
 
-public typealias BKSendDataCompletionHandler = ((data: NSData, remotePeer: BKRemotePeer, error: BKError?) -> Void)
+public typealias BKSendDataCompletionHandler = ((data: Data, remotePeer: BKRemotePeer, error: BKError?) -> Void)
 
 public class BKPeer {
-
+    
     /// The configuration the BKCentral object was started with.
     public var configuration: BKConfiguration? {
         return nil
     }
-
+    
     internal var connectedRemotePeers: [BKRemotePeer] {
-        get {
-            return _connectedRemotePeers
-        }
-        set {
-            _connectedRemotePeers = newValue
-        }
+        return _connectedRemotePeers
     }
-
+    internal var _connectedRemotePeers: [BKRemotePeer] = []
     internal var sendDataTasks: [BKSendDataTask] = []
-
-    private var _connectedRemotePeers: [BKRemotePeer] = []
-
+    
     /**
      Sends data to a connected remote central.
      - parameter data: The data to send.
      - parameter remotePeer: The destination of the data payload.
      - parameter completionHandler: A completion handler allowing you to react in case the data failed to send or once it was sent succesfully.
      */
-    public func sendData(data: NSData, toRemotePeer remotePeer: BKRemotePeer, completionHandler: BKSendDataCompletionHandler?) {
+    public func sendData(_ data: Data, toRemotePeer remotePeer: BKRemotePeer, completionHandler: BKSendDataCompletionHandler?) {
         guard connectedRemotePeers.contains(remotePeer) else {
-            completionHandler?(data: data, remotePeer: remotePeer, error: BKError.RemotePeerNotConnected)
+            completionHandler?(data: data, remotePeer: remotePeer, error: BKError.remotePeerNotConnected)
             return
         }
         let sendDataTask = BKSendDataTask(data: data, destination: remotePeer, completionHandler: completionHandler)
@@ -63,41 +40,41 @@ public class BKPeer {
             processSendDataTasks()
         }
     }
-
+    
     internal func processSendDataTasks() {
         guard sendDataTasks.count > 0 else {
             return
         }
         let nextTask = sendDataTasks.first!
         if nextTask.sentAllData {
-            let sentEndOfDataMark = sendData(configuration!.endOfDataMark, toRemotePeer: nextTask.destination)
-            if sentEndOfDataMark {
-                sendDataTasks.removeAtIndex(sendDataTasks.indexOf(nextTask)!)
-                nextTask.completionHandler?(data: nextTask.data, remotePeer: nextTask.destination, error: nil)
+            let sentEndOfDataMark = sendData(configuration!.endOfDataMark as Data, toRemotePeer: nextTask.destination)
+            if (sentEndOfDataMark) {
+                sendDataTasks.remove(at: sendDataTasks.index(of: nextTask)!)
+                nextTask.completionHandler?(data: nextTask.data as Data, remotePeer: nextTask.destination, error: nil)
                 processSendDataTasks()
             } else {
                 return
             }
         }
         let nextPayload = nextTask.nextPayload
-        let sentNextPayload = sendData(nextPayload, toRemotePeer: nextTask.destination)
+        let sentNextPayload = sendData(nextPayload as Data, toRemotePeer: nextTask.destination)
         if sentNextPayload {
-            nextTask.offset += nextPayload.length
+            nextTask.offset += nextPayload.count
             processSendDataTasks()
         } else {
             return
         }
     }
-
-    internal func failSendDataTasksForRemotePeer(remotePeer: BKRemotePeer) {
+    
+    internal func failSendDataTasksForRemotePeer(_ remotePeer: BKRemotePeer) {
         for sendDataTask in sendDataTasks.filter({ $0.destination == remotePeer }) {
-            sendDataTasks.removeAtIndex(sendDataTasks.indexOf(sendDataTask)!)
-            sendDataTask.completionHandler?(data: sendDataTask.data, remotePeer: sendDataTask.destination, error: .RemotePeerNotConnected)
+            sendDataTasks.remove(at: sendDataTasks.index(of: sendDataTask)!)
+            sendDataTask.completionHandler?(data: sendDataTask.data as Data, remotePeer: sendDataTask.destination, error: .remotePeerNotConnected)
         }
     }
-
-    internal func sendData(data: NSData, toRemotePeer remotePeer: BKRemotePeer) -> Bool {
+    
+    internal func sendData(_ data: Data, toRemotePeer remotePeer: BKRemotePeer) -> Bool {
         fatalError("Function must be overridden by subclass")
     }
-
+    
 }
