4c5b2ec7e03e9539030eaf9f4611814845041de4
Caleb Kleveter <caleb.kleveter@gmail.com>
2016-07-07 08:44:17 -0500

    Updated to Swift 3

--- Source/BKErrorDomain.swift
+++ Source/BKErrorDomain.swift
@@ -1,25 +1,9 @@
 //
+//  BKErrorDomain.swift
 //  BluetoothKit
 //
-//  Copyright (c) 2015 Rasmus Taulborg Hummelmose - https://github.com/rasmusth
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
+//  Created by Rasmus Taulborg Hummelmose on 12/05/2016.
+//  Copyright © 2016 Rasmus Taulborg Hummelmose. All rights reserved.
 //
 
 import Foundation
@@ -31,9 +15,9 @@ import Foundation
     - RemotePeerNotConnected: The action failed because the remote peer attempted to interact with, was not connected.
     - InternalError(underlyingError): Will be returned if any of the internal or private classes returns an unhandled error.
  */
-public enum BKError: ErrorType {
-    case InterruptedByUnavailability(cause: BKUnavailabilityCause)
-    case FailedToConnectDueToTimeout
-    case RemotePeerNotConnected
-    case InternalError(underlyingError: ErrorType?)
+public enum BKError: ErrorProtocol {
+    case interruptedByUnavailability(cause: BKUnavailabilityCause)
+    case failedToConnectDueToTimeout
+    case remotePeerNotConnected
+    case internalError(underlyingError: ErrorProtocol?)
 }
