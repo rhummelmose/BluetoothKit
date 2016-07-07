b5c66c572bf45d56d7a00fe2cf65955e4fea6df1
Caleb Kleveter <caleb.kleveter@gmail.com>
2016-07-07 07:30:26 -0500

    Updated to Swift 3

--- Source/BKConfiguration.swift
+++ Source/BKConfiguration.swift
@@ -29,42 +29,42 @@ import CoreBluetooth
     Class that represents a configuration used when starting a BKCentral object.
 */
 public class BKConfiguration {
-
+    
     // MARK: Properties
-
+    
     /// The UUID for the service used to send data. This should be unique to your applications.
     public let dataServiceUUID: CBUUID
-
+    
     /// The UUID for the characteristic used to send data. This should be unique to your application.
     public var dataServiceCharacteristicUUID: CBUUID
-
+    
     /// Data used to indicate that no more data is coming when communicating.
-    public var endOfDataMark: NSData
-
+    public var endOfDataMark: Data
+    
     /// Data used to indicate that a transfer was cancellen when communicating.
-    public var dataCancelledMark: NSData
-
+    public var dataCancelledMark: Data
+    
     internal var serviceUUIDs: [CBUUID] {
         let serviceUUIDs = [ dataServiceUUID ]
         return serviceUUIDs
     }
-
+    
     // MARK: Initialization
 
-    public init(dataServiceUUID: NSUUID, dataServiceCharacteristicUUID: NSUUID) {
-        self.dataServiceUUID = CBUUID(NSUUID: dataServiceUUID)
-        self.dataServiceCharacteristicUUID = CBUUID(NSUUID: dataServiceCharacteristicUUID)
-        endOfDataMark = "EOD".dataUsingEncoding(NSUTF8StringEncoding)!
-        dataCancelledMark = "COD".dataUsingEncoding(NSUTF8StringEncoding)!
+    public init(dataServiceUUID: UUID, dataServiceCharacteristicUUID: UUID) {
+        self.dataServiceUUID = CBUUID(nsuuid: dataServiceUUID)
+        self.dataServiceCharacteristicUUID = CBUUID(nsuuid: dataServiceCharacteristicUUID)
+        endOfDataMark = "EOD".data(using: String.Encoding.utf8)!
+        dataCancelledMark = "COD".data(using: String.Encoding.utf8)!
     }
-
+    
     // MARK Functions
-
-    internal func characteristicUUIDsForServiceUUID(serviceUUID: CBUUID) -> [CBUUID] {
+    
+    internal func characteristicUUIDsForServiceUUID(_ serviceUUID: CBUUID) -> [CBUUID] {
         if serviceUUID == dataServiceUUID {
             return [ dataServiceCharacteristicUUID ]
         }
         return []
     }
-
+    
 }
