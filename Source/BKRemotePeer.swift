7165b536ca54386083bf651f7fd5ae4d793eb0c3
Caleb Kleveter <caleb.kleveter@gmail.com>
2016-07-07 08:46:45 -0500

    Updated to Swift 3

--- Source/BKRemotePeer.swift
+++ Source/BKRemotePeer.swift
@@ -1,25 +1,9 @@
 //
+//  BKRemotePeer.swift
 //  BluetoothKit
 //
-//  Copyright (c) 2015 Rasmus Taulborg Hummelmose - https://github.com/rasmusth
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
+//  Created by Rasmus Taulborg Hummelmose on 12/05/2016.
+//  Copyright © 2016 Rasmus Taulborg Hummelmose. All rights reserved.
 //
 
 import Foundation
@@ -30,44 +14,44 @@ public protocol BKRemotePeerDelegate: class {
      - parameter remotePeripheral: The remote peripheral that sent the data.
      - parameter data: The data it sent.
      */
-    func remotePeer(remotePeer: BKRemotePeer, didSendArbitraryData data: NSData)
+    func remotePeer(_ remotePeer: BKRemotePeer, didSendArbitraryData data: Data)
 }
 
-public func == (lhs: BKRemotePeer, rhs: BKRemotePeer) -> Bool {
-    return lhs.identifier.isEqual(rhs.identifier)
+public func ==(lhs: BKRemotePeer, rhs: BKRemotePeer) -> Bool {
+    return (lhs.identifier == rhs.identifier)
 }
 
 public class BKRemotePeer: Equatable {
-
+    
     /// A unique identifier for the peer, derived from the underlying CBCentral or CBPeripheral object, or set manually.
-    public let identifier: NSUUID
-
+    public let identifier: UUID
+    
     public weak var delegate: BKRemotePeerDelegate?
-
+    
     internal var configuration: BKConfiguration?
     private var data: NSMutableData?
-
-    init(identifier: NSUUID) {
+    
+    init(identifier: UUID) {
         self.identifier = identifier
     }
-
+    
     internal var maximumUpdateValueLength: Int {
         return 20
     }
-
-    internal func handleReceivedData(receivedData: NSData) {
-        if receivedData.isEqualToData(configuration!.endOfDataMark) {
+    
+    internal func handleReceivedData(_ receivedData: Data) {
+        if receivedData == configuration!.endOfDataMark {
             if let finalData = data {
-                delegate?.remotePeer(self, didSendArbitraryData: finalData)
+                delegate?.remotePeer(self, didSendArbitraryData: finalData as Data)
             }
             data = nil
             return
         }
         if let existingData = data {
-            existingData.appendData(receivedData)
+            existingData.append(receivedData)
             return
         }
-        data = NSMutableData(data: receivedData)
+        data = NSData(data: receivedData) as! NSMutableData
     }
-
+    
 }
