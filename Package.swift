1f9a1c9c8f640d5e6a9994b539395590e2443a15
Yueyu Zhao <>
2023-09-23 14:26:08 +0900

    fix swift package path

--- Package.swift
+++ Package.swift
@@ -25,7 +25,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BluetoothKit",
-            dependencies: []),
+            dependencies: [],
+            path: "Source"
+        ),
         .testTarget(
             name: "BluetoothKitTests",
             dependencies: ["BluetoothKit"]),
